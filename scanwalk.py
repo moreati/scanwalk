7596342060542f2c53375e749f1ea117bbee47c4
Alex Willmer <alex@moreati.org.uk>
2022-08-01 17:41:02 +0100

    Ad type hints

--- scanwalk.py
+++ scanwalk.py
@@ -1,38 +1,45 @@
+from __future__ import annotations
+
 import os
 import stat
+from typing import AnyStr
+from typing import Generator
 
 
 class FakeDirEntry(os.PathLike):
     '''
     A stand-in for os.DirEntry, that can be instantiated directly.
     '''
-    def __init__(self, path):
+    def __init__(self, path:os.PathLike):
         self.path = path
 
-    def __fspath__(self):
-        return self.path
-
     @property
-    def name(self):
+    def name(self) -> AnyStr:
         return os.path.basename(self.path)
 
-    def is_dir(self, *, follow_symlinks=True):
+    def is_dir(self, *, follow_symlinks:bool=True) -> bool:
         return stat.S_ISDIR(self.stat(follow_symlinks=follow_symlinks).st_mode)
 
-    def is_file(self, *, follow_symlinks=True):
+    def is_file(self, *, follow_symlinks:bool=True) -> bool:
         return stat.S_ISREG(self.stat(follow_symlinks=follow_symlinks).st_mode)
 
     def is_symlink(self):
         return stat.S_ISLNK(self.stat(follow_symlinks=False).st_mode)
 
-    def stat(self, *, follow_symlinks=True):
+    def stat(self, *, follow_symlinks:bool=True) -> os.stat_result:
         return os.stat(self.path, follow_symlinks=follow_symlinks)
 
-    def __repr__(self):
+    def __fspath__(self) -> AnyStr:
+        return os.fspath(self.path)
+
+    def __repr__(self) -> str:
         return f'<{self.__class__.__name__} {self.path!r}>'
 
 
-def walk(top, *, follow_symlinks=False):
+WalkGenerator = Generator[os.DirEntry|FakeDirEntry, None, None]
+
+
+def walk(top:os.PathLike, *, follow_symlinks:bool=False) -> WalkGenerator:
     """
     Generate DirEntry objects for top, and directories/files under top
     (excluding '.' and '..' entries).
@@ -47,7 +54,7 @@ def walk(top, *, follow_symlinks=False):
     yield from _walk(top, follow_symlinks=follow_symlinks)
 
 
-def _walk(path, *, follow_symlinks=False):
+def _walk(path:os.PathLike, *, follow_symlinks:bool=False) -> WalkGenerator:
     with os.scandir(path) as it:
         for entry in it:
             skip = yield entry
@@ -57,3 +64,8 @@ def _walk(path, *, follow_symlinks=False):
                 yield from _walk(entry)
 
 
+__all__ = (
+    FakeDirEntry.__name__,
+    WalkGenerator.__name__,
+    walk.__name__,
+)
